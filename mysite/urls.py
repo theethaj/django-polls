ecd93622979d493594442ba124ca3eb5fbb05069
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2019-11-30 22:33:15 +0700

    Update settings and urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 """mysite URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -21,6 +20,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
