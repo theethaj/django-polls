4190e556989fbcd45fc476264af16b6a60955dd3
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2019-11-30 22:34:18 +0700

    Update admin, models, urls and views

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Choice, Question
 
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -17,9 +16,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -42,15 +39,15 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.", })
-
+            'error_message': "You didn't select a choice.",
+        })
     else:
         selected_choice.votes += 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    return HttpResponseRedirect(reverse('/polls/'))
-
-def index(request):
-    return HttpResponseRedirect(reverse("polls:index"))
